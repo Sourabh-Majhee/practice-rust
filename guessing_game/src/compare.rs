0e180e2ea2d1e48571b8084705b12ff45aa2f397
Sourabh Majhee <sourabhmajhee605@gmail.com>
2025-10-25 20:39:52 +0530

    added comparison method to the main program

--- guessing_game/src/compare.rs
+++ guessing_game/src/compare.rs
@@ -5,7 +5,14 @@ use rand::Rng;
 
 fn main(){
     // --snip--
+
+    let mut guess = String::new();
+
     println!("You guessed: {guess}");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}")
 
     match guess.cmp(&secret_number){
         Ordering::Less => println!("Too small!"),
